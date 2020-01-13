68f8f0a7132f84ca3fc328c65f7d33899cced45f
Sanjay <sivasanjay7@gmail.com>
2020-01-13 17:38:24 +0530

    Added restorecontent during refresh

--- shoppingitems.js
+++ shoppingitems.js
@@ -2,8 +2,7 @@ const newItem = document.querySelector(".newItem");
 const list = document.querySelector(".items");
 const shoppingForm = document.querySelector(`[name='shoppingDetails']`);
 
-const items = []; // state of the user
-
+let items = []; // state of the user
 function handleEvent(event) {
   event.preventDefault();
   const name = event.currentTarget.newItem.value;
@@ -19,29 +18,35 @@ function handleEvent(event) {
 function displayItem() {
   const html = items
     .map(data => {
-      return `<li>${data.name}</li>`;
+      return `<li>${data.name} <button type='button' id=${data.id}>Delete</button></li> `;
     })
     .join("");
   list.innerHTML = html;
 }
 
 function storeDatainLocalStorage() {
-  console.log(JSON.parse(localStorage.getItem("items")));
-  localStorage.setItem("items", JSON.stringify(items));
+  localStorage.setItem("items", JSON.stringify(items)); // Its creates a new key:value pair, everytime its called.
   console.log("Stored in Local Storage");
 }
 
-function userState() {
+function restoreUserState() {
   let state = JSON.parse(localStorage.getItem("items"));
   if (state) {
     state.forEach(item => items.push(item));
-    console.log("Triggering!");
-    list.dispatchEvent(new CustomEvent("itemUpdated"));
+    shoppingForm.dispatchEvent(new CustomEvent("restoredItems"));
   }
 }
 
+function deleteItem(event) {
+  const newitems = items.filter(item => item.id != event.target.id);
+  items = newitems;
+  console.log(items);
+  list.dispatchEvent(new CustomEvent("itemUpdated"));
+}
+
 shoppingForm.addEventListener("submit", handleEvent);
+shoppingForm.addEventListener("restoredItems", displayItem);
 list.addEventListener("itemUpdated", displayItem);
 list.addEventListener("itemUpdated", storeDatainLocalStorage);
-// list.addEventListener("itemUpdated", userState);
-userState();
+list.addEventListener("click", deleteItem);
+restoreUserState();
